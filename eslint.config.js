9ab13adbdf8b56aecd16930454c45b71afd6b6a7
ElizabethT7 <tymofieievai@gmail.com>
2024-12-09 12:19:36 +0300

    feat: add libs, prettier, eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import config from "@esfront/eslint-config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
+  ...config,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react"],
+            [
+              "^@mui/base",
+              "^@mui/material/styles",
+              "^@mui/system",
+              "^@mui/material",
+              "^@mui/utils",
+            ],
+            ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+            ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ],
+        },
       ],
     },
   },
-)
+  {
+    ignores: ["node_modules/*"],
+  },
+];
